59517854cade12a0d70e45d5c2be12d62575843c
Varsha P <varshap.ec19@bmsce.ac.in>
2022-01-30 15:57:45 +0530

    Create find element occuring once when all others are present thrice.cpp

---
+++ Arrays/find element occuring once when all others are present thrice.cpp
@@ -0,0 +1,44 @@
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) 
+    {
+        sort(arr,arr+N);
+        for(int i=0;i<N;i+=3)
+        {
+            if(arr[i]!=arr[i+1] || arr[i+1]!=arr[i+2])
+            {
+                return arr[i];
+            }
+        }
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int arr[N];
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.singleElement(arr,N);
+        cout<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
