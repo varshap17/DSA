1e7a9af3a66b3de81e43b685ce4d5489ccf80664
Varsha P <varshap.ec19@bmsce.ac.in>
2022-02-02 21:09:51 +0530

    Create print anagrams together.cpp

---
+++ Hashing/print anagrams together.cpp
@@ -0,0 +1,59 @@
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) 
+    {
+        int n=string_list.size();
+        vector<vector<string>> ans;
+        map<string,vector<string>> m;
+        for(int i=0;i<n;i++)
+        {
+            string a=string_list[i];
+            sort(a.begin(),a.end());
+            m[a].push_back(string_list[i]);
+        }
+        for(auto x:m)
+        {
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
