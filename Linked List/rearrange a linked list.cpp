086cb1103bed75dcb2e700e0942483ad94a116a0
Varsha P <varshap.ec19@bmsce.ac.in>
2022-02-26 00:04:26 +0530

    Create rearrange a linked list.cpp

---
+++ Linked List/rearrange a linked list.cpp
@@ -0,0 +1,95 @@
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+ // } Driver Code Ends
+/* Node structure
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    void rearrangeEvenOdd(Node *head)
+    {
+        Node *qq=head->next;
+        Node *p=head;
+        Node *q=head->next;
+        while(p->next && q->next)
+        {
+            p->next=q->next;
+            p=p->next;
+            q->next=p->next;
+            q=q->next;
+        }
+        p->next=qq;
+    }
+};
+
+
+// { Driver Code Starts.
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,i,n,l;
+
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL;
+        struct Node *temp = head;
+        cin>>n;
+        for(i=1; i<=n; i++)
+        {
+            cin>>l;
+
+            if (head == NULL)
+            {   
+                head = temp = new Node(l); 
+                
+            }
+            else
+            {  temp->next = new Node(l);
+               temp = temp->next;
+            }
+        }
+        Solution ob;
+        ob.rearrangeEvenOdd(head);
+        while(head != NULL)
+        {
+            printf("%d ", head->data);
+            head = head->next;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
