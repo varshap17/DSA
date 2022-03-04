7e8e4ad847c6a341d0c6fa493fcf4e98519a57ad
Varsha P <varshap.ec19@bmsce.ac.in>
2022-03-04 19:11:17 +0530

    Create implement stack using linked list.cpp

---
+++ Stacks/implement stack using linked list.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StackNode {
+    int data;
+    StackNode *next;
+    StackNode(int a) {
+        data = a;
+        next = NULL;
+    }
+};
+
+class MyStack {
+  private:
+    StackNode *top;
+
+  public:
+    void push(int);
+    int pop();
+    MyStack() { top = NULL; }
+};
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        MyStack *sq = new MyStack();
+
+        int Q;
+        cin >> Q;
+        while (Q--) {
+            int QueryType = 0;
+            cin >> QueryType;
+            if (QueryType == 1) {
+                int a;
+                cin >> a;
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
+
+
+//Function to push an integer into the stack.
+void MyStack ::push(int x) 
+{
+    StackNode *t=new StackNode(x);
+    if(top==NULL)
+    {
+        t->next=NULL;
+        top=t;
+    }
+    else
+    {
+        t->next=top;
+        top=t;
+    }
+    
+}
+
+
+//Function to remove an item from top of the stack.
+int MyStack ::pop() 
+{
+    if(top==NULL)
+    {
+        return -1;
+    }
+    else
+    {
+        int temp=top->data;
+        top=top->next;
+        return temp;
+    }
+}
