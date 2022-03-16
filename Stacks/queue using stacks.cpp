255e4845f317ce2078cd4b650767d37d3b7be9fc
Varsha P <varshap.ec19@bmsce.ac.in>
2022-03-16 18:50:29 +0530

    Create queue using stacks.cpp

---
+++ Stacks/queue using stacks.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Queue {
+    stack<int> input, output;
+public:
+
+    void enqueue(int x) 
+    {
+        while(output.size())
+        {
+            input.push(output.top());
+            output.pop();
+        }
+        input.push(x);
+    }
+
+    int dequeue() 
+    {
+        if(input.size()!=0)
+        {
+            while(input.size())
+            {
+                output.push(input.top());
+                input.pop();
+            }
+            int y=output.top();
+            output.pop();
+            return y;
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Queue ob;
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            ob.enqueue(a);
+        }
+        else if(QueryType==2)
+        {
+            cout<<ob.dequeue()<<" ";
+
+        }
+        }
+    cout<<endl;
+    }
+}
+  // } Driver Code Ends
