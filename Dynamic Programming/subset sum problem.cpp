fac37a9bd48af49f5984cdd91f123555117a5690
Varsha P <varshap.ec19@bmsce.ac.in>
2022-03-18 19:50:31 +0530

    Create subset sum problem.cpp

---
+++ Dynamic Programming/subset sum problem.cpp
@@ -0,0 +1,60 @@
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    bool isSubsetSum(int N, int arr[], int sum)
+    {
+        bool t[N+1][sum+1];
+        for(int i=0;i<N+1;i++)
+        {
+            t[i][0]=true;
+        }
+        for(int j=1;j<sum+1;j++)
+        {
+            t[0][j]=false;
+        }
+        for(int i=1;i<N+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    t[i][j]=t[i-1][j-arr[i-1]]||t[i-1][j];
+                }
+                else
+                {
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[N][sum];
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, sum;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cin >> sum;
+        
+        Solution ob;
+        cout << ob.isSubsetSum(N, arr, sum) << endl;
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
