4de847633e4bfd21fd523cfe5cfab1ea773370f8
Varsha P <varshap.ec19@bmsce.ac.in>
2022-04-04 21:36:57 +0530

    Create Rotation.cpp

---
+++ Searching/Rotation.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) 
+	{
+	    int i=0;
+	    while(arr[i]>arr[n-1])
+	    {
+	        i++;
+	    }
+	    return i;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findKRotation(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
