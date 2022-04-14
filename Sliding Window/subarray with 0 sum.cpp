97a1fe84d6cec5860ceb0548ef232cd2c5a20c33
Varsha P <varshap.ec19@bmsce.ac.in>
2022-04-14 10:28:14 +0530

    Create subarray with 0 sum.cpp

---
+++ Sliding Window/subarray with 0 sum.cpp
@@ -0,0 +1,52 @@
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        map<int,int> m;
+        int pre[n];
+        pre[0]=arr[0];
+        m[pre[0]]++;
+        for(int i=1;i<n;i++)
+        {
+            pre[i]=pre[i-1]+arr[i];
+            if(m.find(pre[i])!=m.end() || pre[i]==0)
+            {
+                return true;
+            }
+            m[pre[i]]++;
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}  // } Driver Code Ends
