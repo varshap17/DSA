b1a42d421978b743d336ba24ba02f9ac3eb98d39
Varsha P <varshap.ec19@bmsce.ac.in>
2022-04-22 19:27:20 +0530

    Create median in a row wise sorted matrix.cpp

---
+++ Matrices/median in a row wise sorted matrix.cpp
@@ -0,0 +1,46 @@
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int median(vector<vector<int>> &matrix, int r, int c)
+    {
+        vector<int> a;
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                a.push_back(matrix[i][j]);
+            }
+        }
+        sort(a.begin(),a.end());
+        int k=(r*c)/2;
+        return a[k];
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, c;
+        cin>>r>>c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for(int i = 0; i < r; ++i)
+            for(int j = 0;j < c; ++j)
+                cin>>matrix[i][j];
+        Solution obj;
+        cout<<obj.median(matrix, r, c)<<endl;        
+    }
+    return 0;
+}  // } Driver Code Ends
