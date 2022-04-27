aca10573fb380a0615bbee8b41a13d5d1f94446b
Varsha P <varshap.ec19@bmsce.ac.in>
2022-04-27 06:20:59 +0530

    Create killing spree.cpp

---
+++ Math/killing spree.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution {
+public:
+    long long int killinSpree(long long int n)
+    {
+        int i=1;
+        long long int c=0;
+        while(n>=i*i )
+        {
+            n=n-(i*i);
+            c++;
+            i++;
+        }
+        return c;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n; cin>>n;
+        Solution obj;
+        long long int ans = obj.killinSpree(n);
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
