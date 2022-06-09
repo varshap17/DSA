d6524b944e5ef910c8f7581dc672b0eafdc18323
Varsha P <varshap.ec19@bmsce.ac.in>
2022-06-09 06:46:50 +0530

    Create convert a string to its equivalent mobile equivalent.cpp

---
+++ Strings/convert a string to its equivalent mobile equivalent.cpp
@@ -0,0 +1,39 @@
+string printSequence(string S)
+{
+    unordered_map <char,string> mapping;
+    mapping['A']="2";
+    mapping['B']="22";
+    mapping['C']="222";
+    mapping['D']="3";
+    mapping['E']="33";
+    mapping['F']="333";
+    mapping['G']="4";
+    mapping['H']="44";
+    mapping['I']="444";
+    mapping['J']="5";
+    mapping['K']="55";
+    mapping['L']="555";
+    mapping['M']="6";
+    mapping['N']="66";
+    mapping['O']="666";
+    mapping['P']="7";
+    mapping['Q']="77";
+    mapping['R']="777";
+    mapping['S']="7777";
+    mapping['T']="8";
+    mapping['U']="88";
+    mapping['V']="888";
+    mapping['W']="9";
+    mapping['X']="99";
+    mapping['Y']="999";
+    mapping['Z']="9999";
+    mapping[' ']="0";
+    
+    string ans;
+    
+    for(int i=0;i<S.length();i++)
+    {
+        ans=ans+mapping[S[i]];
+    }
+    return ans;
+}
