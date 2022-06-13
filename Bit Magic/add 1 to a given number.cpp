e93067cda33641224dd7e442fd0d487cd12f1ba1
Varsha P <varshap.ec19@bmsce.ac.in>
2022-06-13 21:55:40 +0530

    Create add 1 to a given number.cpp

---
+++ Bit Magic/add 1 to a given number.cpp
@@ -0,0 +1,16 @@
+int addOne(int x)
+{
+    int m = 1;
+     
+    // Flip all the set bits
+    // until we find a 0
+    while( x & m )
+    {
+        x = x ^ m;
+        m <<= 1;
+    }
+     
+    // flip the rightmost 0 bit
+    x = x ^ m;
+    return x;
+}
